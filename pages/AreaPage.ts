1855f11e5923f934ccb9291e7c891ca9aa7d788c
Alfred <alfred.intal@gmail.com>
2025-03-11 11:30:36 +0800

    Finished t04

--- pages/AreaPage.ts
+++ pages/AreaPage.ts
@@ -16,33 +16,47 @@ export class AreaPage {
             await this.page.getByRole('button', {name: 'Save'}).click();
         }
     }
-    async editAndDeleteArea()
+    async editArea(name : string, newName: string)
     {
-        // Set a name to be used as reference for the new Area
-        const newName = 'Name Edited ' + Date.now().toString();
         // Select the first created Area
-        const row = this.page.locator('table tr', { has: this.page.locator('td', { hasText: 'Name 0' }) });
+        const row = this.page.locator('table tr', { has: this.page.locator('td', { hasText: name }) });
         await row.getByRole('button', {name: 'Edit'}).click();
         await this.page.locator('#Name').fill(newName);
         await this.page.locator('#ShortName').fill('ShortName Edited');
         await this.page.locator('#ExportCode').fill('ExportCode Edited');
         await this.page.getByRole('button', {name: 'Save'}).click();
+    }
+    async deleteArea(newName : string)
+    {
         // Locate the updated Area
         const newRow = this.page.locator('table tr', { has: this.page.locator('td', { hasText: newName }) });
         await newRow.getByRole('button', {name: 'Delete'}).click();
         await this.page.locator('#yesButton').click();
-
-        this.unDeleteArea(newName);
     }
-
     async unDeleteArea(newName : string)
     {
         await this.page.locator('a', {hasText: 'UnDelete'}).click();
-        await this.page.locator('a[aria-label="Go to the last page"]').click();
+        // Wait for the Undelete window to open
+        await expect(this.page.locator('#undeleteDlg_wnd_title')).toBeVisible();
+        // Utilize the filter function to narrow down to the unique name (based on Epoch)
+        await this.page.getByLabel('UnDelete').getByRole('columnheader', { name: 'Filter Name' }).getByLabel('Filter').click();
+        await this.page.locator('input[data-bind="value:filters[0].value"]').fill(newName);
+        await this.page.getByRole('button', { name: 'Filter' }).click();
+        // Use filters to confirm if the element is present on the Undelete table
+        // Assert if the row exists after filtering   
         const row = this.page.locator('table tr', { has: this.page.locator('td', { hasText: newName }) });
         await row.getByRole('button', {name: 'Restore'}).click();
         await this.page.locator('button', {hasText: 'Close'}).click();
     }
-
+    async checkArea(newName)
+    {
+        // Use filters to confirm if the element has been restored
+        // Assert if the row exists after filtering
+        await this.page.locator('#GridAreas').getByRole('columnheader', { name: 'Filter Name' }).getByLabel('Filter').click();
+        const openForm = this.page.locator('form[aria-hidden="false"]');
+        await openForm.locator('input[data-bind="value:filters[0].value"]').fill(newName);
+        await openForm.getByRole('button', { name: 'Filter' }).click();
+        expect(this.page.locator('table tr', { has: this.page.locator('td', { hasText: newName }) })).toBeVisible();
+    }
 }
 
--- tests/t04-verify-admin-crud-operations-in-area-module.test.ts
+++ tests/t04-verify-admin-crud-operations-in-area-module.test.ts
@@ -7,14 +7,15 @@ test.use({ headless: false });
 
 test('verify admin CRUD operations in Area module', async ({page}) => {
     const loginPage = new LoginPage(page);   
-    const areaPage = new AreaPage(page); 
+    const areaPage = new AreaPage(page);
     await loginPage.navigate(process.env.BASE_URL!);
     await loginPage.login(process.env.ADMIN_ID!, process.env.PASSWORD!, process.env.ADMIN_NAME!);
 
     await areaPage.navigate();
-
+    const newName = 'Name Edited + ' + Date.now().toString();
     await areaPage.createArea();
-
-    await areaPage.editAndDeleteArea();
-
+    await areaPage.editArea('Name 0', newName);
+    await areaPage.deleteArea(newName);
+    await areaPage.unDeleteArea(newName);
+    await areaPage.checkArea(newName);
 })
