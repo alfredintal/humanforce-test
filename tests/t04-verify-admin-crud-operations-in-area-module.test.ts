5b4fddf52955bbbeb2c232d8b4fadb101d9fc3e9
Alfred <alfred.intal@gmail.com>
2025-03-11 21:00:46 +0800

    added comments on t04

--- tests/t04-verify-admin-crud-operations-in-area-module.test.ts
+++ tests/t04-verify-admin-crud-operations-in-area-module.test.ts
@@ -11,8 +11,10 @@ test('verify admin CRUD operations in Area module', async ({page}) => {
     await loginPage.login(process.env.ADMIN_ID!, process.env.PASSWORD!, process.env.ADMIN_NAME!);
 
     await areaPage.navigate();
+    // Store a name that will be used for edit, delete, undelete, and checks
     const newName = 'Name Edited + ' + Date.now().toString();
     await areaPage.createArea();
+    // Edit, Delete, Undelete, and Check workflows
     await areaPage.editArea('Name 0', newName);
     await areaPage.deleteArea(newName);
     await areaPage.unDeleteArea(newName);
