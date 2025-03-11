2d987e1e0c93f0ac1b14bff639301485df88bbfd
Alfred <alfred.intal@gmail.com>
2025-03-11 10:39:11 +0800

    Completed area create, update, delete. Working on undelete.

--- pages/AreaPage.ts
+++ pages/AreaPage.ts
@@ -8,7 +8,40 @@ export class AreaPage {
     }
     async createArea()
     {
+        for (let i = 0; i < 3; i++) {
+            await this.page.locator('a', {hasText: 'Add new record'}).click();
+            await this.page.locator('#Name').fill('Name ' + i);
+            await this.page.locator('#ShortName').fill('ShortName ' + i);
+            await this.page.locator('#ExportCode').fill('ExportCode ' + i);
+            await this.page.getByRole('button', {name: 'Save'}).click();
+        }
+    }
+    async editAndDeleteArea()
+    {
+        // Set a name to be used as reference for the new Area
+        const newName = 'Name Edited ' + Date.now().toString();
+        // Select the first created Area
+        const row = this.page.locator('table tr', { has: this.page.locator('td', { hasText: 'Name 0' }) });
+        await row.getByRole('button', {name: 'Edit'}).click();
+        await this.page.locator('#Name').fill(newName);
+        await this.page.locator('#ShortName').fill('ShortName Edited');
+        await this.page.locator('#ExportCode').fill('ExportCode Edited');
+        await this.page.getByRole('button', {name: 'Save'}).click();
+        // Locate the updated Area
+        const newRow = this.page.locator('table tr', { has: this.page.locator('td', { hasText: newName }) });
+        await newRow.getByRole('button', {name: 'Delete'}).click();
+        await this.page.locator('#yesButton').click();
 
+        this.unDeleteArea(newName);
+    }
+
+    async unDeleteArea(newName : string)
+    {
+        await this.page.locator('a', {hasText: 'UnDelete'}).click();
+        await this.page.locator('a[aria-label="Go to the last page"]').click();
+        const row = this.page.locator('table tr', { has: this.page.locator('td', { hasText: newName }) });
+        await row.getByRole('button', {name: 'Restore'}).click();
+        await this.page.locator('button', {hasText: 'Close'}).click();
     }
 
 }
--- tests/t04-verify-admin-crud-operations-in-area-module.test.ts
+++ tests/t04-verify-admin-crud-operations-in-area-module.test.ts
@@ -1,11 +1,20 @@
 import {test, expect} from '@playwright/test';
 import dotenv from 'dotenv';
 import { LoginPage } from '../pages/LoginPage';
+import { AreaPage } from '../pages/AreaPage';
 dotenv.config();
 test.use({ headless: false }); 
 
 test('verify admin CRUD operations in Area module', async ({page}) => {
-    const loginPage = new LoginPage(page);    
+    const loginPage = new LoginPage(page);   
+    const areaPage = new AreaPage(page); 
     await loginPage.navigate(process.env.BASE_URL!);
     await loginPage.login(process.env.ADMIN_ID!, process.env.PASSWORD!, process.env.ADMIN_NAME!);
+
+    await areaPage.navigate();
+
+    await areaPage.createArea();
+
+    await areaPage.editAndDeleteArea();
+
 })
